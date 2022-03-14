d6e5540537f102b9c094560fc8095fb8b5fb211f
Ezgi Safçı <ezgi.safci@onur.net>
2022-03-14 14:23:41 +0300

    validations update

--- src/app/components/components/sign-up/sign-up.component.ts
+++ src/app/components/components/sign-up/sign-up.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import {FormGroup, FormBuilder} from "@angular/forms";
+import {FormGroup, FormBuilder, Validators} from "@angular/forms";
 import { Router } from '@angular/router';
 @Component({
   selector: 'app-sign-up',
@@ -16,10 +16,10 @@ export class SignUpComponent implements OnInit {
   ngOnInit(): void {
 
     this.signUpForm = this.formBuilder.group({
-      fullName:[''],
-      email:[''],
-      username:[''],
-      passwd:['']
+      fullName:['',Validators.required],
+      email:['',Validators.required],
+      username:['',Validators.required],
+      passwd:['',Validators.required]
     })
 
 
